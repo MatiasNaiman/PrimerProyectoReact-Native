2f2e5ceea6e4a0b12c3d7fe25e6ebdb17348b013
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-01 09:06:35 -0300

    holi

--- App.js
+++ App.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 import FlatlistTragos from './src/components/FlatlistTragos';
 
+
+const FotoDeFondo = require('./assets/background/playita.jpg')
+const FotoDeFondo2 = require('./assets/background/betsson.png')
+
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Tragos:</Text>
-      <FlatlistTragos />
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <ImageBackground source={FotoDeFondo} resizemode='cover' style={styles.backgroundImage}>
+        <Text style={styles.titulo}>La tequileria del Furro:</Text>
+        <FlatlistTragos style={styles.text}/>
+        <StatusBar style="auto" />
+        <Image source={FotoDeFondo2} style={styles.image}></Image>
+        </ImageBackground>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  titulo:{
+    left:15,
+    alignItems: 'center',
+    textAlign: 'left',
+    fontSize: 25,
+    fontStyle:'italic',
+    marginBottom: 10,
+    marginTop: 15,
+  },
+  container:{
+    marginTop: 50,
     display: 'flex',
     flex: 1,
     width:'center',
-    backgroundColor: '#00000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    
+    justifyContent: 'left',
   },
+  backgroundImage: {
+    flex: 1, 
+  },
+  image: {
+    width: '100%'
+  }
+
 });
--- src/components/FlatlistTragos.js
+++ src/components/FlatlistTragos.js
@@ -6,14 +6,14 @@ const FlatlistTragos = () => {
     <View>
       <FlatList
         data={[
-          { key: 'Jhonny Walker Blue Label' },
-          { key: 'Jhonny Walker Red Label' },
-          { key: 'Jhonny Walker Black Label' },
-          { key: 'Jhonny Walker Gold Label' },
-          { key: 'Caipiroshka de Frutos Rojos' },
-          { key: 'Fernet Cola' },
-          { key: 'Malibu de Coco' },
-          { key: 'Licor de Melon con Speed' },
+          { key: '• Jhonny Walker Blue Label' },
+          { key: '• Jhonny Walker Red Label' },
+          { key: '• Jhonny Walker Black Label' },
+          { key: '• Jhonny Walker Gold Label' },
+          { key: '• Caipiroshka de Frutos Rojos' },
+          { key: '• Fernet Cola' },
+          { key: '• Malibu de Coco' },
+          { key: '• Licor de Melon con Speed' },
         ]}
         renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
       />
@@ -22,6 +22,10 @@ const FlatlistTragos = () => {
 };
 
 const styles = StyleSheet.create({
+  item: {
+    left: 15,
+    fontSize: 15,
+  }
 });
 
 export default FlatlistTragos;
