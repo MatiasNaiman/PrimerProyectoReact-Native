94cd2173e6a54675e76b5e4e5d4d7ff5aea5d683
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-01 09:18:51 -0300

    holi

--- App.js
+++ App.js
@@ -3,48 +3,47 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 import FlatlistTragos from './src/components/FlatlistTragos';
 
-
-const FotoDeFondo = require('./assets/background/playita.jpg')
-const FotoDeFondo2 = require('./assets/background/betsson.png')
+const FotoDeFondo = require('./assets/background/playita.jpg');
+const FotoDeFondo2 = require('./assets/background/betsson.png');
 
 export default function App() {
-
   return (
-      <View style={styles.container}>
-        <ImageBackground source={FotoDeFondo} resizemode='cover' style={styles.backgroundImage}>
+    <View style={styles.container}>
+      <ImageBackground source={FotoDeFondo} resizeMode="cover" style={styles.backgroundImage}>
         <Text style={styles.titulo}>La tequileria del Furro:</Text>
-        <FlatlistTragos style={styles.text}/>
+        <FlatlistTragos style={styles.text} />
         <StatusBar style="auto" />
-        <Image source={FotoDeFondo2} style={styles.image}></Image>
-        </ImageBackground>
-      </View>
+        <Image source={FotoDeFondo2} resizeMode="contain" style={styles.image} />
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titulo:{
-    left:15,
+  titulo: {
+    left: 15,
     alignItems: 'center',
     textAlign: 'left',
     fontSize: 25,
-    fontStyle:'italic',
+    fontStyle: 'italic',
     marginBottom: 10,
     marginTop: 15,
   },
-  container:{
+  container: {
     marginTop: 50,
     display: 'flex',
     flex: 1,
-    width:'center',
+    width: 'center',
     alignItems: 'left',
-    
     justifyContent: 'left',
   },
   backgroundImage: {
-    flex: 1, 
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
   image: {
-    width: '100%'
-  }
-
+    width: '100%', // Adjust the width here, e.g., '100%' or a specific number like 300
+    height: '100%', // Adjust the height here, e.g., '100%' or a specific number like 200
+  },
 });
